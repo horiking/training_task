d3aed3e98030e98d0d41e2fff2273414b2231e11
horiking <frederik.wollny@outlook.de>
2025-07-30 12:25:05 +0200

    updated pom

--- trading/src/main/java/com/trader/trading/model/ReconciliationDiff.java
+++ trading/src/main/java/com/trader/trading/model/ReconciliationDiff.java
@@ -1,6 +1,10 @@
 package com.trader.trading.model;
-
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class ReconciliationDiff {
@@ -8,10 +12,8 @@ public class ReconciliationDiff {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String tradeId;
     private String fieldName;
-
     private String valueSystemA;
     private String valueSystemB;
 
