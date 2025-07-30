eb87c0168222966abf3045b37ac4b31b420e4225
Gabriel <rizangabriel3@gmail.com>
2025-07-30 16:13:41 +0300

    crossorigin set on instrument

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:9090/api",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
