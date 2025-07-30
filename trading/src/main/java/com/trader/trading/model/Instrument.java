715611c56cc52e8561108e320059492d0477f557
Gabriel <rizangabriel3@gmail.com>
2025-07-30 15:29:25 +0300

    instrument endpoint done

--- trading/src/main/java/com/trader/trading/model/Instrument.java
+++ trading/src/main/java/com/trader/trading/model/Instrument.java
@@ -14,4 +14,36 @@ public class Instrument {
     private String isin;
 
     // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIsin() {
+        return isin;
+    }
+
+    public void setIsin(String isin) {
+        this.isin = isin;
+    }
 }
